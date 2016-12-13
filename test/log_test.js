6f067360291134e04103a6e30ac21820846916ee
zt.melody <zt.freedom@gmail.com>
2016-12-13 20:55:07 +0800

    更新API文档

--- test/log_test.js
+++ test/log_test.js
@@ -4,24 +4,24 @@
     'use strict'
 var logger = require('../logger');
 var Enumerable = require('linq');
-//logger.t("trace");
-//logger.d("debug");
-//logger.i("info");
-//logger.w("warn");
-//logger.e('test error');
-//logger.e(new Error('test error'));
+logger.t("trace");
+logger.d("debug");
+logger.i("info");
+logger.w("warn");
+logger.e('test error');
+logger.e(new Error('test error'));
 
-
-var client=[];
-client["a"] = {'id':1,'data':'1'};
-client["b"] = {'id':2,'data':'2'};
-//console.log(client);
-//delete client["c"];
-//console.log(client);
-
-console.log(client.length);
-if(client.length){
-    console.log('1');
-}else{
-    console.log('2');
-}
+//
+//var client=[];
+//client["a"] = {'id':1,'data':'1'};
+//client["b"] = {'id':2,'data':'2'};
+////console.log(client);
+////delete client["c"];
+////console.log(client);
+//
+//console.log(client.length);
+//if(client.length){
+//    console.log('1');
+//}else{
+//    console.log('2');
+//}
