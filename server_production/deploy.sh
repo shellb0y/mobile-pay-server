8cef4c41646f852bb4a00fe59e5cd7c74214c0a8
zt.melody <zt.freedom@gmail.com>
2016-12-24 16:42:17 +0800

    redis部署

--- server_production/deploy.sh
+++ server_production/deploy.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-if
+
+if [ $1 = -dr ]; then
+docker run --name mbs_redis -p 6379:6379 -v redis.conf:/usr/local/etc/redis/redis.conf -v /var/redis/data:/data \
+ --restart=on-failure:5 -d daocloud.io/library/redis:3.2.5 redis-server /usr/local/etc/redis/redis.conf
+else
+echo 12321321
+fi
