9581971b27d261488bb4379af7e5357c7a4f6363
zt.melody <zt.freedom@gmail.com>
2016-12-24 20:24:18 +0800

    更新deploy.sh

--- server_production/deploy.sh
+++ server_production/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 if [ $1 = -dr ]; then
-docker run --name mbs_redis -p 6379:6379 -v redis.conf:/usr/local/etc/redis/redis.conf -v /var/redis/data:/data \
- --restart=on-failure:5 -d daocloud.io/library/redis:3.2.5 redis-server /usr/local/etc/redis/redis.conf
+docker run --name mbs_redis -p 6379:6379 \
+  -v /usr/local/src/mobile-pay-server/server_production/redis.conf:/usr/local/etc/redis/redis.conf -v /var/redis/data:/data \
+  --restart=on-failure:5 -d daocloud.io/library/redis:3.2.5 redis-server /usr/local/etc/redis/redis.conf
 else
 echo 12321321
 fi
