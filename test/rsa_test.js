cf3803fdd132e0ecf34113b357cd4dc3396ca520
zt.melody <zt.freedom@gmail.com>
2017-01-11 14:51:49 +0800

    增加account modified

--- test/rsa_test.js
+++ test/rsa_test.js
@@ -1,3 +1,6 @@
+require('../date_ex');
+
 var t = new Date().getTime() - 30 * 60 * 1000;
 console.log(t);
-console.log(new Date(t));
+console.log(new Date());
+console.log(new Date().format('yyyy-MM-dd hh:mm:ss'));
